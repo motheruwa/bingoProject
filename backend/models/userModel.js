ad9ae0453dddf5d2e964a77aeb4c115d91916be0
estifanos <estifanosassalif19@gmail.com>
2024-10-25 18:00:44 +0100

    first-commit

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
 
 const userSchema = new Schema({
     userName: {
@@ -48,10 +47,7 @@ const userSchema = new Schema({
             throw new Error('Email already in use');
         }
 
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-
-        const user = await this.create({ userName, password: hash, balance, permission});
+        const user = await this.create({ userName, password, balance, permission});
         return user;
     } catch (error) {
         console.error('Error in user signup:', error.message);
@@ -59,19 +55,18 @@ const userSchema = new Schema({
     }
 };
 
-  userSchema.statics.login = async function (userName, password) {
-    if (!userName || !password) {
+userSchema.statics.login = async function (userName, password) {
+  if (!userName || !password) {
       throw Error('All fields must be filled');
-    }
-    const user = await this.findOne({ userName });
-    if (!user) {
+  }
+  const user = await this.findOne({ userName });
+  if (!user) {
       throw Error('User not found');
-    }
-    const match = await bcrypt.compare(password, user.password);
-    if (!match) {
+  }
+  if (user.password !== password) {
       throw Error('Incorrect password');
-    }
-    return user;
-  };
+  }
+  return user;
+};
 
 module.exports = mongoose.model('User', userSchema);
