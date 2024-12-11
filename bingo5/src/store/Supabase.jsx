966599b1b3491092c55f01f4a22cb7e2ddb123e1
estifanos <estifanosassalif19@gmail.com>
2024-12-11 09:48:04 +0000

    first-commit

--- bingo5/src/store/Supabase.jsx
+++ bingo5/src/store/Supabase.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://wgwtfmebkklzwkghykop.supabase.co'
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indnd3RmbWVia2tsendrZ2h5a29wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzAxODM0NDcsImV4cCI6MjA0NTc1OTQ0N30.Td0DQ1XuUpe6M8w23JZaMTws4GCoNwhpYIuaOUGyCLE"
+const supabaseUrl = 'https://czuwmutuqgoyhrwtmrio.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dXdtdXR1cWdveWhyd3RtcmlvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5MDc3MDcsImV4cCI6MjA0OTQ4MzcwN30.KHe4r1IPTIOiShzenJ7b8ij-qZvROD8ziR8F6yf4aNg"
 export  const supabase = createClient(supabaseUrl, supabaseKey)
