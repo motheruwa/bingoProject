62b1e967355fc5635eb0cb7ab43adae40321ff55
estifanos assalif <101446429+motheruwa@users.noreply.github.com>
2025-03-08 11:46:08 +0300

    Update

--- bingocard100/src/page/StartBingo.jsx
+++ bingocard100/src/page/StartBingo.jsx
@@ -1883,9 +1883,9 @@ const StartBingo = () => {
     <div className={styles.container}>
       <div className={styles.bounce}><BouncingBalls/></div>
       <div className={styles.link}>
-          <button onClickCapture={handleregisterClick} disabled={creatingReport} >RegisterCard</button>
-        <button onClickCapture={handleReportClick} disabled={creatingReport} >Report</button>
-        <button onClick={handleLogOut} disabled={creatingReport} >Logout</button>
+          <button onClickCapture={handleregisterClick} >RegisterCard</button>
+        <button onClickCapture={handleReportClick} >Report</button>
+        <button onClick={handleLogOut}  >Logout</button>
       </div>
       <div className={styles.card}>
         <img src={BingoCard} alt="Bingo Card" />
