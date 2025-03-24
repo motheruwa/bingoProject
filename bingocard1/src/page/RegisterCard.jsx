0a6d900deb0e447218ad56a466e94e93022cad6b
estifanos <estifanosassalif19@gmail.com>
2025-03-24 19:03:51 +0000

    first-commit

--- bingocard1/src/page/RegisterCard.jsx
+++ bingocard1/src/page/RegisterCard.jsx
@@ -146,33 +146,16 @@ const RegisterCard = () => {
               value={selectedAmount}
               onChange={handleAmountChange}
             >
-             {fetchedUser.userName !== "tedaaa" ? (
-                <>
-                  <option value={10}>በ 10</option>
-                  <option value={20}>በ 20</option>
-                  <option value={30}>በ 30</option>
-                  <option value={40}>በ 40</option>
-                  <option value={50}>በ 50</option>
-                  <option value={100}>በ 100</option>
-                  <option value={200}>በ 200</option>
-                  <option value={300}>በ 300</option>
-                  <option value={400}>በ 400</option>
-                  <option value={500}>በ 500</option>
-                </>
-              ) : (
-                <>
-                  <option value={10} disabled>በ 10</option>
-                  <option value={20}>በ 20</option>
-                  <option value={30}>በ 30</option>
-                  <option value={40}>በ 40</option>
-                  <option value={50}>በ 50</option>
-                  <option value={100}>በ 100</option>
-                  <option value={200}>በ 200</option>
-                  <option value={300}>በ 300</option>
-                  <option value={400}>በ 400</option>
-                  <option value={500}>በ 500</option>
-                </>
-              )}
+             <option value={10}>በ 10</option>
+                           <option value={20}>በ 20</option>
+                           <option value={30}>በ 30</option>
+                           <option value={40}>በ 40</option>
+                           <option value={50}>በ 50</option>
+                           <option value={100}>በ 100</option>
+                           <option value={200}>በ 200</option>
+                           <option value={300}>በ 300</option>
+                           <option value={400}>በ 400</option>
+                           <option value={500}>በ 500</option>
             </select>
           </div>
 
