91bbcb0bc61a35e0d2ab11e9e48a41a2f16a511e
motheruwa <estifanosassalif19@gmail.com>
2025-06-27 13:27:15 -0700

    first-commit

--- xbingo1/src/App.js
+++ xbingo1/src/App.js
@@ -2,13 +2,12 @@ import { Route, Routes } from 'react-router-dom';
 import AdminPage from './page/AdminPage';
 import User from './page/User';
 import SignUp from './page/SignUp';
-import Login from './page/Login';
 
 function App() {
   return (
     <div>
      <Routes>
-      <Route path='/' element={<Login/>}/>
+      <Route path='/' element={<AdminPage/>}/>
       <Route path='/adminmrxbingobingo' element={<AdminPage/>}/>
       <Route path='/user/:username' element={<User/>}/>
       <Route path='/add' element={<SignUp/>}/>
